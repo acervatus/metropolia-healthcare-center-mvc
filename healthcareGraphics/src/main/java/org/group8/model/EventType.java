3f7b7e3c65172b3513e03e0050e16f403dc728c5
acervatus <142780750+acervatus@users.noreply.github.com>
2024-09-26 10:46:11 +0300

    mvc healthcare-center

---
+++ healthcareGraphics/src/main/java/org/group8/model/EventType.java
@@ -0,0 +1,7 @@
+package org.group8.model;
+
+import org.group8.framework.IEventType;
+
+public enum EventType implements IEventType {
+    ARR_CHECKIN, DEP_CHECKIN, DEP_DOCTOR, DEP_LAB, DEP_XRAY, DEP_TREATMENT;
+}
