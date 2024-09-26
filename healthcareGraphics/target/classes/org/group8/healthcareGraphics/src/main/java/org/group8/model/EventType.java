6c5a9e548f0bcefd8b6175a6c71cffe111f2d2a6
acervatus <142780750+acervatus@users.noreply.github.com>
2024-09-26 11:13:18 +0300

    Delete target directory

--- healthcareGraphics/target/classes/org/group8/healthcareGraphics/src/main/java/org/group8/model/EventType.java
+++
@@ -1,7 +0,0 @@
-package org.group8.model;
-
-import org.group8.framework.IEventType;
-
-public enum EventType implements IEventType {
-    ARR_CHECKIN, DEP_CHECKIN, DEP_DOCTOR, DEP_LAB, DEP_XRAY, DEP_TREATMENT;
-}
